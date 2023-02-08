636a895b53c40891c44df45568a2a76d3bc924c6
Timmy1291 <timmysharp1291@hotmail.com>
2023-02-08 07:59:50 -0600

    Fixed rotations not running

--- Systemcontroller.sh
+++ Systemcontroller.sh
@@ -15,7 +15,7 @@ do
 	cur=$(($count+$2))
 	count=$(($3+$count))
 	echo "Controller running on ${array[$cur]}"
-	python bashrun.py sys.txt --exp_index "${array[$cur]}"
+	python System_Parser_SC.py @sys.txt --exp_index "${array[$cur]}"
 	
 done
 
