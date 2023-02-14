d6ad8116251e447c51bed234c3fd0dd8f6848e3d
Timmy1291 <timmysharp1291@hotmail.com>
2023-02-14 08:01:35 -0600

    Fixed some updated test partition code

--- System_Parser_SC.py
+++ System_Parser_SC.py
@@ -92,7 +92,7 @@ def exp_type_to_hyperparameters(args):
     elif args.exp_type =='sys_sweep_SIR_10':  
         p = {'P_fa':[0.0001,0.0002,0.0003,0.0004,0.0005,0.0006,0.0007,0.0008,0.0009,0.001,0.002,0.003,0.004,0.005,0.006,0.007,0.008,0.009,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1],
              'rotation':[1,2,3,4,5,6,7,8,9,10]}
-        args.partition=10 if args.exp_index < 140 else 50 if args.exp_index < 740 else 200
+        args.partition=10 if args.exp_index < 100 else 50 if args.exp_index < 550 else 200
     elif args.exp_type =='sys_sweep':        
         p = {'P_fa':[0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1],
              'rotation':[1,2,3,4,5,6,7,8,9,10],
