0c80dec267a46607a3d7097ee6c30a9e501c16c8
RyanF02 <rf20f@fsu.edu>
2023-07-06 11:45:18 -0500

    Detection Model Isolation Update
    Each Detection Model now runs in its own container. FA_CD / FA_GLRT / FA_IDEAL containers have been removed. Now each model runs sequentially and processes the data to calculate the false alarm rates for each.

--- metacogtainer/README_system_guide.txt
+++ metacogtainer/README_system_guide.txt
@@ -8,6 +8,10 @@ This will allow you to manage created Images and Containers easier.
 
 This will allow you to easily create and run Containers.
 
+If you do not have the required libraries (tensorflow, numpy, mat73...):
+	Navigate to the Dynamic-CFAR/metacogtainer folder
+	Run the following command: pip install -r requirements.txt
+
 If you are unable to run commands from the terminal, try starting Docker Desktop if you	have not already.
 
 (c) Lastly, you'll need the code. Download the Dynamic-CFAR GitHub folder to your computer.
@@ -21,17 +25,21 @@ OPTIONAL: To make the code run faster, you can copy and run the following block 
 docker build -t metacog-base -f Dockerfile_base . ; 
 docker build -t metacog-discriminator -f Dockerfile_disc . ; 
 docker build -t metacog-evaluator -f Dockerfile_eval . ; 
-docker build -t metacog-fa-cd -f Dockerfile_fa_cd . ; 
-docker build -t metacog-fa-glrt -f Dockerfile_fa_glrt . ; 
-docker build -t metacog-fa-ideal -f Dockerfile_fa_ideal . ; 
-docker build -t metacog-results -f Dockerfile_results .
+docker build -t model-0 -f Model0_dockerfile . ;
+docker build -t model-1 -f Model1_dockerfile . ;
+docker build -t model-2 -f Model2_dockerfile . ;
+docker build -t model-3 -f Model3_dockerfile . ;
+docker build -t model-4 -f Model4_dockerfile . ;
+docker build -t model-5 -f Model5_dockerfile . ;
+docker build -t model-6 -f Model6_dockerfile . ;
+docker build -t metacog-results -f Dockerfile_results . ;
 
 (2) Run the following command: python python-setup-script.py
 
 (3) The output will appear in the terminal after all the containers have run.
-    For explanations of the commands, please view system_setup_commands.txt
+    For explanations of the commands, please view setup_commands_all.txt
     
-    Docker MetaCog Code.pdf contains explanations for changes made to the python code
-    Docker MetaCog Containers.pdf contains explanations for the overall Container system
+    Docker MetaCog Code.pdf contains explanations for changes made to the python code  - OUTDATED
+    Docker MetaCog Containers.pdf contains explanations for the overall Container system - OUTDATED
 
 --------------------------------------------------------------------------------------------------------
--- metacogtainer/python-setup-script.py
+++ metacogtainer/python-setup-script.py
@@ -4,8 +4,10 @@ import docker
 # Gets access to Docker from Python
 client = docker.from_env()
 
+print("\n------------------------------------\n")
+
 # Creates Volume for long term storage
-print("Creating MetaCog Volume...", )
+print("MetaCog File Volume Ready...", )
 client.volumes.create(name='metacog-volume')
 
 # Checks if Docker images exist, if not they are built before continuing
@@ -15,7 +17,7 @@ except docker.errors.ImageNotFound:
     print("Building MetaCog Base...")
     client.images.build(path='.', tag='metacog-base', dockerfile='Dockerfile_base')
 
-print("MetaCog Base Ready...")
+print("MetaCog Base Image Ready...")
 
 try: 
     client.images.get('metacog-discriminator')
@@ -30,29 +32,56 @@ except docker.errors.ImageNotFound:
     client.images.build(path='.', tag='metacog-evaluator', dockerfile='Dockerfile_eval')
     
 try: 
-    client.images.get('metacog-fa-cd')
+    client.images.get('model-0')
 except docker.errors.ImageNotFound:
-    print("Building MetaCog FA-CD...")
-    client.images.build(path='.', tag='metacog-fa-cd', dockerfile='Dockerfile_fa_cd')
-    
+    print("Building Model-0...")
+    client.images.build(path='.', tag='model-0', dockerfile='Model0_dockerfile')
+
 try: 
-    client.images.get('metacog-fa-glrt')
+    client.images.get('model-1')
 except docker.errors.ImageNotFound:
-    print("Building MetaCog FA-GLRT...")
-    client.images.build(path='.', tag='metacog-fa-glrt', dockerfile='Dockerfile_fa_glrt')
-    
+    print("Building Model-1...")
+    client.images.build(path='.', tag='model-1', dockerfile='Model1_dockerfile')
+
 try: 
-    client.images.get('metacog-fa-ideal')
+    client.images.get('model-2')
 except docker.errors.ImageNotFound:
-    print("Building MetaCog FA-IDEAL...")
-    client.images.build(path='.', tag='metacog-fa-ideal', dockerfile='Dockerfile_fa_ideal')
+    print("Building Model-2...")
+    client.images.build(path='.', tag='model-2', dockerfile='Model2_dockerfile')
+
+try: 
+    client.images.get('model-3')
+except docker.errors.ImageNotFound:
+    print("Building Model-3...")
+    client.images.build(path='.', tag='model-3', dockerfile='Model3_dockerfile')
+
+try: 
+    client.images.get('model-4')
+except docker.errors.ImageNotFound:
+    print("Building Model-4...")
+    client.images.build(path='.', tag='model-4', dockerfile='Model4_dockerfile')
+
+try: 
+    client.images.get('model-5')
+except docker.errors.ImageNotFound:
+    print("Building Model-5...")
+    client.images.build(path='.', tag='model-5', dockerfile='Model5_dockerfile')
+
+try: 
+    client.images.get('model-6')
+except docker.errors.ImageNotFound:
+    print("Building Model-6...")
+    client.images.build(path='.', tag='model-6', dockerfile='Model6_dockerfile')
     
 try: 
     client.images.get('metacog-results')
 except docker.errors.ImageNotFound:
     print("Building MetaCog Results...")
     client.images.build(path='.', tag='metacog-results', dockerfile='Dockerfile_results')
-        
+
+print("\n---------- Setup Complete ----------\n")
+
+
 # Runs each container sequentially
 print("Running discriminator...")
 client.containers.run('metacog-discriminator', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
@@ -60,17 +89,29 @@ client.containers.run('metacog-discriminator', volumes={'metacog-volume': {'bind
 print("Running evaluator...")
 client.containers.run('metacog-evaluator', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
 
-print("Running FA-CD...")
-client.containers.run('metacog-fa-cd', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
+print("Reading data with Model-0...")
+client.containers.run('model-0', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
 
-print("Running FA-GLRT...")
-client.containers.run('metacog-fa-glrt', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
+print("Reading data with Model-1...")
+client.containers.run('model-1', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
 
-print("Running FA-IDEAL...")
-client.containers.run('metacog-fa-ideal', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
+print("Reading data with Model-2...")
+client.containers.run('model-2', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
 
-print("--- Process Complete ---")
+print("Reading data with Model-3...")
+client.containers.run('model-3', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
 
-print("Gathering results...")
+print("Reading data with Model-4...")
+client.containers.run('model-4', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
+
+print("Reading data with Model-5...")
+client.containers.run('model-5', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
+
+print("Reading data with Model-6...")
+client.containers.run('model-6', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}})
+
+print("\n--------- Process Complete ---------\n")
+
+print("Gathering results...\n")
 results = client.containers.run('metacog-results', volumes={'metacog-volume': {'bind': '/app/docker_bind', 'mode': 'rw'}}, stdout=True)
-print( results[0:-1] ) # leaves out \n from string
+print( results.decode() ) # leaves out \n from string
