ab7d9b5c07f74c1db5796dfff2106af415371000
lesya17 <122303202+lesya17@users.noreply.github.com>
2023-09-18 13:46:04 -0500

    Update Docker_Evaluator.py

--- metacogtainer/Containers/Evaluator/Docker_Evaluator.py
+++ metacogtainer/Containers/Evaluator/Docker_Evaluator.py
@@ -1,7 +1,15 @@
 # For each probability distribution finds the max (most likely distribution) and saves to new file
 
 # args.max_test is the number of tests that run. If args.max_test is None that the whole data set will be trained.
+# args.show_output is as show_output in Args_Class_Module.Args_Class()
 # data_ss is a data array with a reduse by one dim
+# disc_vector is a vector with the numbers each of which corresponds to certain distribution. In our case, each disc_vector
+#   has 7 elements and the total number of disc_vector is the length of  data_ss or args.max_test
+# max_disc_list is the list with the the indices of the maximum values in each disc_vector
+
+## output
+# max_disc_list.csv in /app/docker_bind/max_disc_list.csv
+###
 
 # for ML building AI models
 import tensorflow as tf
@@ -42,15 +50,21 @@ else:
     # create column-array with len(data_ss) zeros elements
     max_disc_list = np.zeros(shape=(len(data_ss), 1))
 
-# load distribution_tensors.csv
+# load distribution_tensors.csv that was created in Dockerfile_disc wher Docker_Discriminator.py was running
 distribution_tensors = loadtxt('/app/docker_bind/distribution_tensors.csv', delimiter=',')
 #print(distribution_tensors)
 
+#*****************************************************
+# Creates a list, max_disc_list, with the indices
+#   of the maximum values in each disc_vector
 i = 0
 for disc_vector in distribution_tensors:
+     # Returns the indices of the maximum values
      max_disc = np.argmax(disc_vector)
+
      max_disc_list[i] = max_disc
      i = i + 1
+##*****************************************************
 
 if args.show_output is True:
     print(max_disc_list)
